ce9b33ecb67ebc067b662028013d2347d7faf604
musa küçük <musa.kucuk99@gmail.com>
2022-07-26 22:11:51 +0300

    base keyword is added

---
+++ Base() Keyword/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BaseKeyword
+{
+    internal class Program 
+    {
+        static void Main() 
+        {
+            // Using base() keyword we can access and use 
+            Car car = new("Audi");
+            Console.WriteLine(car.GetBrandOfVehicle);
+        }
+    }
+}
+
+class Vehicle 
+{
+    private string brand;
+    public Vehicle(string brand)
+    {
+        this.brand = brand;
+    }
+
+    public string GetBrandOfVehicle { 
+        get { return this.brand; } 
+    }
+}
+
+class Car : Vehicle
+{
+    public Car(string brand) : base(brand)
+    {
+
+    }
+}
