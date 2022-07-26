c01a30ad0da9dcd79d569c5157c8be229f15807a
musa küçük <musa.kucuk99@gmail.com>
2022-07-26 21:38:25 +0300

    Name Hiding is added

---
+++ Name Hidden/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace NameHidden
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+           // There is "Write the console method" in two class. both same.
+           // But it will run in child class  
+           B b = new B();
+           b.WriteTheConsole();
+        }
+    }
+}
+
+class A
+{
+    public void WriteTheConsole()
+    {
+        Console.WriteLine("Class A");
+    }
+}
+
+class B : A
+{
+    public void WriteTheConsole()
+    {
+        Console.WriteLine("Class B");
+    }
+}
+
