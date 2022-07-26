675846cd44a082b5fe8aec903c506addc4542af7
musa küçük <musa.kucuk99@gmail.com>
2022-07-26 21:25:47 +0300

    Polimorphism  is added

---
+++ Polimorphism/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Polimorphism 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // You can show if you write "b.". You will access just the two method X and Y. 
+            B b = new C();
+            // b.
+        }   
+    }
+}
+
+class A
+{
+    public void X() { }
+}
+class B : A
+{
+    public void Y() { }
+}
+class C : B
+{
+    public void Z() { }
+}
