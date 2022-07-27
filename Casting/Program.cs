fd6e35d860f85175688aa4e0a3c5374888a49799
musa küçük <musa.kucuk99@gmail.com>
2022-07-27 22:49:04 +0300

    Casting is added

---
+++ Casting/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Casting
+{
+    internal class MyApp 
+    {
+        static void Main() 
+        {
+            // We can convert with casting 
+            C c = new C();
+            B b = (B)c;
+            b.WriteForB();
+        }
+    }
+}
+
+class A 
+{
+    public void WriteForA() 
+    {
+        Console.WriteLine("A");
+    }
+}
+
+class B : A 
+{
+    public void WriteForB() 
+    {
+        Console.WriteLine("B");
+    }
+}
+
+class C : B 
+{
+    public void WriteForC() 
+    {
+        Console.WriteLine("C");
+    }
+}
